b04ba19da3d16c723d82c9cd90aace70264f83a4
imen <imenganfoudi123@gmail.com>
2026-09-20 17:29:25 +0200

    Enable API Platform on all entities (Client, Product, Devis, Facture)

--- src/Entity/Facture.php
+++ src/Entity/Facture.php
@@ -4,7 +4,9 @@ namespace App\Entity;
 
 use App\Repository\FactureRepository;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
 
+#[ApiResource]
 #[ORM\Entity(repositoryClass: FactureRepository::class)]
 class Facture
 {
